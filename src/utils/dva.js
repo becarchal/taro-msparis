30956587e42f32b15e2925ab3d1c159d0abe6f3e
杨斌 <Bin2.Yang@five-star.cn>
2019-04-08 23:36:06 +0800

    添加model持久化插件用Taro本地缓存做storage

--- src/utils/dva.js
+++ src/utils/dva.js
@@ -1,17 +1,38 @@
+import Taro from '@tarojs/taro'
 import { create } from 'dva-core';
 import { createLogger } from 'redux-logger';
 import createLoading from 'dva-loading';
+import { persistEnhancer } from 'dva-model-persist';
 
 let app;
 let store;
 let dispatch;
 
+const storage = {
+  get: Taro.getStorageSync,
+  set: Taro.setStorageSync,
+}
 function createApp(opt) {
   // redux日志
   // opt.onAction = [createLogger()];
+  // 1. Initialize
   app = create(opt);
   app.use(createLoading({}));
-
+  app.use({
+    // model持久化插件
+    extraEnhancers: [
+      persistEnhancer({
+        // key: 'model', 默认model
+        storage, // 默认session
+        // blacklist: [ // 黑名单
+        //   '@@dva',
+        //   'routing'
+        // ], // 默认值
+        // whitelist: ['cdmGlobal'] // 白名单
+        // keyPrefix: 'persist', // 默认值
+      })
+    ]
+  });
   if (!global.registered) opt.models.forEach(model => app.model(model));
   global.registered = true;
   app.start();
